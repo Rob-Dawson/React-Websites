68c3f0b94a96c5b860bad22b7d8e8f35b99f6a6f
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-05 18:17:09 +0000

    Included items, pictures, styling

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,9 +1,135 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useEffect, useState } from "react";
+import "./index.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
 function App() {
-  return <h1>Hello React</h1>;
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+      </div>
+      <span>{props.price + 3}</span>
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Pizza Place</h1>
+    </header>
+  );
+};
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <h2>Our Menu</h2>
+      <Pizza
+        name={pizzaData.at(0).name}
+        ingredient={pizzaData.at(0).ingredients}
+        price={pizzaData.at(0).price}
+        photoName={pizzaData.at(0).photoName}
+        soldOut={pizzaData.at(0).soldOut}
+      />
+      <Pizza
+        name={pizzaData.at(1).name}
+        ingredient={pizzaData.at(1).ingredients}
+        price={pizzaData.at(1).price}
+        photoName={pizzaData.at(1).photoName}
+        soldOut={pizzaData.at(1).soldOut}
+      />
+      <Pizza
+        name={pizzaData.at(2).name}
+        ingredient={pizzaData.at(2).ingredients}
+        price={pizzaData.at(2).price}
+        photoName={pizzaData.at(2).photoName}
+        soldOut={pizzaData.at(2).soldOut}
+      />
+    </div>
+  );
+};
+
+function Time() {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return <span>{time.toLocaleTimeString()}</span>;
+}
+
+const Footer = () => {
+  const currentHour = new Date().getHours();
+  const openHour = 9;
+  const closedHour = 17;
+  const isOpen = currentHour >= openHour && currentHour < closedHour;
+  console.log(isOpen);
+  return (
+    <footer>
+      The current time is: <Time />. We're Open
+    </footer>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
