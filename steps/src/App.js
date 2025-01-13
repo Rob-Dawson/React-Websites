3d89dedbca4409b0f9ec09b1a1d59cfb09d262ea
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-13 18:43:07 +0000

    Seperated App into a component

---
+++ steps/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div>
+      <h1>React</h1>
+    </div>
+  );
+}
--- steps/src/index.js
+++ steps/src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div>
-      <h1>React</h1>
-    </div>
-  );
-}
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
