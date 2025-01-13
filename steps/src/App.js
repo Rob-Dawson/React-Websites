ce223aee3944870ea40aa8191eff9a82b1f74e7a
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-13 19:04:57 +0000

    Created buttons and conditional text based on step variable value. This needs to be changed to update based on onClick handler

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  const step = 1;
   return (
-    <div>
-      <h1>React</h1>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">{messages[step - 1]}</p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#FFF" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#FFF" }}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
