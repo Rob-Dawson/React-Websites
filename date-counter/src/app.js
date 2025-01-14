b4ee659e9ff20a7a356653aa21a7ba4568096345
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-14 12:00:59 +0000

    Added Step counter Counter counter functions to handle increase and decrease Date Date which changes based on the counter

--- date-counter/src/app.js
+++ date-counter/src/app.js
@@ -1,3 +1,67 @@
+import { useState } from "react";
+
 export default function App() {
-  return <h1>React</h1>;
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [counter, setCounter] = useState(0);
+
+  function handlePrevStep() {
+    if (step > 1)
+      setStep((currentStep) => {
+        return (currentStep = step - 1);
+      });
+  }
+
+  function handleNextStep() {
+    setStep((currentStep) => {
+      return (currentStep = step + 1);
+    });
+  }
+
+  function handlePrevCount() {
+    if (counter >= 1) {
+      setCounter((currentCounter) => {
+        return (currentCounter = counter - step);
+      });
+    }
+  }
+
+  function handleNextCount() {
+    setCounter((currentCounter) => {
+      return (currentCounter = step + counter);
+    });
+  }
+
+  function handleDate() {
+    const date = new Date();
+    let days = step * counter;
+    date.setDate(date.getDate() + counter);
+    console.log(days);
+    return date.toDateString();
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={handlePrevStep}>-</button>
+        <span>Step:{step} </span>
+        <button onClick={handleNextStep}>+</button>
+      </div>
+
+      <div>
+        <button onClick={handlePrevCount}>-</button>
+        <span>Counter: {counter}</span>
+        <button onClick={handleNextCount}>+</button>
+      </div>
+
+      <p>Today is {handleDate()}</p>
+    </div>
+  );
 }
