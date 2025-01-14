75fd2f1f96cc934e567698db14e091ec28446c03
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-14 13:05:51 +0000

    Moved date conditions to a named function. Removed step to use currentStep

--- date-counter/src/app.js
+++ date-counter/src/app.js
@@ -15,36 +15,44 @@ function Counter() {
   function handlePrevStep() {
     if (step > 1)
       setStep((currentStep) => {
-        return (currentStep = step - 1);
+        return (currentStep = currentStep - 1);
       });
   }
 
   function handleNextStep() {
     setStep((currentStep) => {
-      return (currentStep = step + 1);
+      return (currentStep = currentStep + 1);
     });
   }
 
   function handlePrevCount() {
     setCounter((currentCounter) => {
-      return (currentCounter = counter - step);
+      return (currentCounter = currentCounter - step);
     });
   }
 
   function handleNextCount() {
     setCounter((currentCounter) => {
-      return (currentCounter = step + counter);
+      return (currentCounter = currentCounter + step);
     });
   }
 
   function handleDate() {
     const date = new Date();
-    let days = step * counter;
     date.setDate(date.getDate() + counter);
-    console.log(days);
     return date.toDateString();
   }
 
+  function dateMessage() {
+    const date = handleDate();
+    if (counter > 0) {
+      return `${counter} days from now is ${date}`;
+    } else if (counter < 0) {
+      return `${Math.abs(counter)} days from ago is ${date}`;
+    } else {
+      return `Today is ${date}`;
+    }
+  }
   return (
     <div>
       <div>
@@ -59,18 +67,7 @@ function Counter() {
         <button onClick={handleNextCount}>+</button>
       </div>
       <div>
-        <p>
-          {(() => {
-            const date = handleDate();
-            if (counter > 0) {
-              return `${counter} days from now is ${date}`;
-            } else if (counter < 0) {
-              return `${Math.abs(counter)} days from ago is ${date}`;
-            } else {
-              return `Today is ${date}`;
-            }
-          })()}
-        </p>
+        <p>{dateMessage()}</p>
       </div>
     </div>
   );
