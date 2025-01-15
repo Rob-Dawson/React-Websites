70b7b0e11c60a69bc9b58216609df7874e0ad0eb
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-15 19:03:35 +0000

    Added some items and outputted them using a map

--- travel-list/src/PackageList.js
+++ travel-list/src/PackageList.js
@@ -1,7 +1,20 @@
+const initialItems = [
+  { id: 1, description: "Passport", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function PackageList() {
   return (
     <div className="list">
-      <ul>asd asdasd asdasd asd</ul>
+      <ul>
+        {initialItems.map((items) => (
+          <Item items={items} />
+        ))}
+      </ul>
     </div>
   );
+}
+
+function Item({ items }) {
+  return <li>{items.description}</li>;
 }
