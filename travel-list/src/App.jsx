80953179ce5b364902e7a0df62c3cfe14453bc6b
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-16 11:37:04 +0000

    Renamed files as jsx

---
+++ travel-list/src/App.jsx
@@ -0,0 +1,14 @@
+import Logo from "./Logo";
+import Form from "./Form";
+import PackageList from "./PackageList";
+import Stats from "./Stats";
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackageList />
+      <Stats />
+    </div>
+  );
+}
