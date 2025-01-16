a751a832a71fd6de7bf7427744368dec2bc52e3a
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-16 11:40:25 +0000

    Renamed to JSX

--- travel-list/src/Logo.js
+++
@@ -1,3 +0,0 @@
-export default function Logo() {
-  return <h1>🏖️React</h1>;
-}
--- travel-list/src/Stats.js
+++
@@ -1,7 +0,0 @@
-export default function Stats() {
-  return (
-    <footer className="stats">
-      <em>You have X items on your list. You already packed X</em>
-    </footer>
-  );
-}
