191a044f96f056967fc1359e89d744c66debb0bd
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-18 20:01:57 +0000

    Created initial layout of the site. Added slider and output the value based on the step. This is static for now and will be updated

--- date-counter-v2/src/App.jsx
+++ date-counter-v2/src/App.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 export default function App() {
   return (
-    <div>
-      <h1>React</h1>
+    <div className="App">
+      <Counter />
     </div>
   );
+
+  function Counter() {
+    const [step, setStep] = useState(4);
+
+    return (
+      <div>
+        <div>
+          <input type="range" min="1" max="10" value={step} />
+          <span></span>
+        </div>
+        <div>
+          <button>-</button>
+          <span>Count</span>
+          <button>+</button>
+        </div>
+        <div>
+          <p>Today is...</p>
+        </div>
+      </div>
+    );
+  }
 }
