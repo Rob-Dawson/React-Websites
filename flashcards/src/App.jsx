827cf051d0fc4e533812a50f0990557e05bbc636
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-18 19:10:41 +0000

    Created questions object And outputting a <p> for every question

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -1,7 +1,55 @@
+const questions = [
+  {
+    id: 3457,
+    question: "What language is react based on?",
+    answer: "Javascript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of react apps?",
+    answer: "Components",
+  },
+
+  {
+    id: 8832,
+    question:
+      "What is the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How do you pass data from a parent to a child component?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How do you give a component memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completley synchronised with ",
+    answer: "Controlled Element",
+  },
+];
+
 export default function App() {
   return (
-    <div>
-      <h1>React</h1>
+    <div className="App ">
+      <Flashcards />
+    </div>
+  );
+}
+
+function Flashcards() {
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div key={question.id}>
+          <p>{question.question}</p>
+        </div>
+      ))}
     </div>
   );
 }
