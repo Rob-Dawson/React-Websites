0b7a5e78414ce3f0ec5a5bd7375da67095e6f91a
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-19 18:27:11 +0000

    Created state for counter form and step slider. onChange fires a function which sets the state.

--- date-counter-v2/src/App.jsx
+++ date-counter-v2/src/App.jsx
@@ -9,16 +9,34 @@ export default function App() {
 
   function Counter() {
     const [step, setStep] = useState(4);
+    const [counter, setCounter] = useState(1);
 
     return (
       <div>
         <div>
-          <input type="range" min="1" max="10" value={step} />
-          <span></span>
+          <input
+            type="range"
+            min="1"
+            max="10"
+            value={step}
+            onChange={(e) => {
+              setStep(e.target.value);
+              console.log(e.target);
+            }}
+          />
+          <span>{step}</span>
         </div>
         <div>
           <button>-</button>
-          <span>Count</span>
+          <input
+            type="text"
+            placeholder="0"
+            value={counter}
+            onChange={(e) => {
+              console.log(e.target);
+              setCounter(e.target.value);
+            }}
+          />
           <button>+</button>
         </div>
         <div>
