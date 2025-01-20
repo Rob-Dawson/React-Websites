7efa3f73da3a68b4cd1d821c6b09853fb55c802a
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-20 18:59:38 +0000

    Lifted items state into App and passed into Form and PackageList components

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -2,12 +2,17 @@ import Logo from "./Logo";
 import Form from "./Form";
 import PackageList from "./PackageList";
 import Stats from "./Stats";
+import { useState } from "react";
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackageList />
+      <Form onAddItems={handleAddItems} />
+      <PackageList items={items} />
       <Stats />
     </div>
   );
